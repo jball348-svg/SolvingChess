9b0a06a8e699a5046c003904055b40484ba285c1
John Fairfax-Ball <jball348@gmail.com>
2026-08-06 10:40:45 +0100

    Implement position encoding for chess moves
    Added function to encode chess position into decision coordinates.

--- experiments/001_decision_coordinates/src/encoder.py
+++ experiments/001_decision_coordinates/src/encoder.py
@@ -1,1 +1,28 @@
+from chess_state import get_legal_moves, get_fen
+from move_ordering import order_moves
 
+
+def encode_position(board, move):
+    """
+    Converts a chess position and chosen move
+    into decision coordinates.
+    """
+
+    legal_moves = get_legal_moves(board)
+
+    ordered_moves = order_moves(
+        board,
+        legal_moves
+    )
+
+    deg_plus = len(ordered_moves)
+
+    chosen_edge = (
+        ordered_moves.index(move) + 1
+    )
+
+    return {
+        "fen": get_fen(board),
+        "deg_plus": deg_plus,
+        "chosen_edge": chosen_edge
+    }

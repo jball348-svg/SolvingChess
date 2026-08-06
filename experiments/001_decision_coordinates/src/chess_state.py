b9101705662eaf74b0bd97d3b25d5ce21a94afb4
John Fairfax-Ball <jball348@gmail.com>
2026-08-06 10:40:09 +0100

    Add functions to get legal moves and FEN representation

--- experiments/001_decision_coordinates/src/chess_state.py
+++ experiments/001_decision_coordinates/src/chess_state.py
@@ -1,1 +1,17 @@
+import chess
 
+
+def get_legal_moves(board):
+    """
+    Returns all legal moves from a chess position.
+    """
+
+    return list(board.legal_moves)
+
+
+def get_fen(board):
+    """
+    Returns the FEN representation of a position.
+    """
+
+    return board.fen()

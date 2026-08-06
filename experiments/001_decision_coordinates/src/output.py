98295c19958a2bbec87426a2ce43d8fc9c02b480
John Fairfax-Ball <jball348@gmail.com>
2026-08-06 10:41:03 +0100

    Implement save_csv function to export records
    Added a function to save records to a CSV file using pandas.

--- experiments/001_decision_coordinates/src/output.py
+++ experiments/001_decision_coordinates/src/output.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+
+def save_csv(records, filename):
+    """
+    Saves encoded positions to CSV.
+    """
+
+    dataframe = pd.DataFrame(records)
+
+    dataframe.to_csv(
+        filename,
+        index=False
+    )
